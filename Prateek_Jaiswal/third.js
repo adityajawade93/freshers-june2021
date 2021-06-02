2a1e106d8a4e6d38a55d233859e712a62b38b83e
unknown <prateek.jaiswal@zopsmart.com>
2021-06-02 12:50:01 +0530

    dublicate

--- Prateek_Jaiswal/third.js
+++ Prateek_Jaiswal/third.js
@@ -1,10 +1,19 @@
 // JavaScript source code
-var arr = [9, 8, 7, 6, 5,4, 3, 2, 1]
+var arr = [9, 8, 7, 8,8,4, 9, 2, 1]
 var i;
-var j;
-
-var temp;
-
+var count;
+count = 0;
 arr.sort((a, b) => b - a);
 
-console.log(arr[2]);
+for (i = 0; i <= 8; ++i)
+{
+    if (arr[i] > arr[i + 1]) {
+        count = count + 1;
+    }
+
+
+    if (count === 3)
+    {
+        console.log(arr[i]);
+    }
+}

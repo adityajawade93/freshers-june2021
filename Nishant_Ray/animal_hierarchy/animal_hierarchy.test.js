9dfb31fd97c5730b8954f652ea3350c7c6c4f3a5
rnishant97 <nishant.ray@zopsmart.com>
2021-06-05 01:03:58 +0530

    adadad

--- Nishant_Ray/animal_hierarchy/animal_hierarchy.test.js
+++
@@ -1,7 +0,0 @@
-//not complete, giving wrong answer
-const {dog,cat}=require('./animal_hierarchy')
-
-test("testcase1",()=>{
-    let animal_1=new cat("billo","breed",24,"mouse");
-    expect(animal_1.animal_name()).toBe("this animal's name isbillo")
-})

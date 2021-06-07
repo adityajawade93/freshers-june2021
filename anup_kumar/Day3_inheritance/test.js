7e0987c140ae3169a623ed0add738be9d952d808
anupjha299 <anup.kumar@zopsmart.com>
2021-06-07 20:56:58 +0530

    added flatten array and inheritance

--- anup_kumar/Day3_inheritance/test.js
+++ anup_kumar/Day3_inheritance/test.js
@@ -1,3 +1,190 @@
+function DoSomething() {}
+
+console.log(DoSomething.prototype)
+
+
+// __proto__ ==> __proto__ ==> __proto__
+
+
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+Person.prototype.sayHello = function() { console.log(`Hello my name is ${this.name}`) }
+
+Person.prototype.sayGoodBye = function() { console.log(`See you again!`) }
+
+Person.prototype.isAdult = function() { return this.age > 18 }
+
+// let p1 = new Person('Alice', 23)
+
+// p1 = Object.create(Person)
+// this.name = name
+// this.age = age
+// this -> p1
+
+// function Professional(name, age, profession) {
+//   Person.call(this, name, age)
+//   this.profession = profession
+// }
+
+// Professional.prototype = Object.create(Person.prototype)
+
+// Object.defineProperty(Professional.prototype, 'constructor', {
+//   value: Professional,
+//   enumerable: false, // so that it does not appear in 'for in' loop
+//   writable: true });
+
+// Professional.prototype.sayProfession = function () {
+//   console.log(`My name is ${this.name} and i am a ${this.profession}`)
+// }
+
+
+// let pro1 = new Professional('aditya', 28, 'SDE')
+
+// pro1.sayProfession()
+
+// pro1.sayHello()
+
+// Class Keyword
+
+// class Person {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+
+//   sayHello() {
+//     console.log('Hello my name is '+ this.name);
+//   }
+
+//   isAdult() {
+//     return this.age > 18;
+//   }
+// }
+
+// let p1 = new Person('aditya', 28);
+
+// p1.sayHello();
+// console.log(p1.isAdult());
+
+// p1.name = 'john'
+// p1.sayHello();
+
+// class Professional extends Person {
+//   constructor(name, age, profession) {
+//     super(name, age);
+//     this.profession = profession
+//   }
+
+//   sayProfession() {
+//     console.log(`My name is ${this.name} and i am a ${this.profession}`)
+//   }
+// }
+
+// let pro1 = new Professional('aditya', 28, 'Software Engineer')
+
+// pro1.sayProfession()
+
+
+// var ref;
+
+// function outer() {
+//   var a = 100;
+//   var b = 900;
+//   var c = 700;
+//   function inner() {
+//     console.log(a)
+//   }
+
+//   function inner2() {
+//     console.log(b)
+//   }
+//   return {
+//     inner2,
+//     inner
+//   }
+// }
+
+// const obj = outer()
+
+// obj.inner()
+// obj.inner2()
+
+
+// function Person(name, age) {
+//   return Object.freeze({
+//     sayHello: function() {
+//       console.log(`Hello My name is ${name} and my age is ${age}`)
+//     }
+//   })
+// }
+
+// let p1 = Person('aditya', 28)
+// p1.sayHello()
+
+// p1.name = 'Jhon'
+// p1.sayHello()
+
+// function doesSomething() { 
+//   var v1 = 10; 
+//   var v2 = 100;
+
+//   console.log(v1, v2)
+// }
+
+// doesSomething()
+
+// function createCounter() {
+//   let counter = 0;
+//   return Object.freeze({
+//     incrementCount: function() {
+//       counter += 1;
+//     },
+//     getCounter: function() {
+//       return counter;
+//     }
+//   })
+// }
+
+// function counters() {
+//   let c1 = createCounter();
+//   let c2 = createCounter();
+
+//   c1.incrementCount()
+//   c1.incrementCount()
+//   c1.incrementCount()
+//   c1.incrementCount()
+//   c1.incrementCount()
+//   c1.incrementCount()
+
+//   console.log(c1.getCounter())
+
+//   c2.incrementCount()
+//   c2.incrementCount()
+//   c2.incrementCount()
+//   c2.incrementCount()
+
+
+//   console.log(c2.getCounter())
+// }
+
+// function dummy() { console.log('Hello') }
+
+// counters()
+// dummy()
+
+
+
+
+
+
+
+
+
+
+
 // var person={
 //     name:"anup",
 //     Age:25,
@@ -181,9 +368,233 @@ var person=
 
 // JavaScript Object Constructors
 
-function Person(first,last,age)
-{
-    this.first=first,
-    this.last=last,
-    this.age=age
+// function Person(first,last,age)
+// {
+//     this.first=first,
+//     this.last=last,
+//     this.age=age
+// }
+
+// var A = new Person("John", "Doe", 50, "blue");
+// var B = new Person("Sally", "Rally", 48, "green");
+// console.log(A,B)
+// In JavaScript, the thing called this is the object that "owns" the code.
+
+// The value of this, when used in an object, is the object itself.
+//////////////////////////////////////////////
+
+// a function can be added to object but not to a object constructor
+
+// function Person(first,last)
+// {
+//     this.first=first;
+//     this.last=last;
+// }
+// var p1=new Person("anup","Jha");
+// Person.nationality="indian"
+// console.log(p1)
+
+
+//Adding a Property to an Object is easy
+//Adding a Method to an Object is easy
+//Adding a Property to a Constructor is not possible
+// You cannot add a new method to an object constructor the same way you add a new method to an existing object.
+//Built in Object
+// var x1 = new Object();    // A new Object object
+// var x2 = new String();    // A new String object
+// var x3 = new Number();    // A new Number object
+// var x4 = new Boolean();   // A new Boolean object
+// var x5 = new Array();     // A new Array object
+// var x6 = new RegExp();    // A new RegExp object
+// var x7 = new Function();  // A new Function object
+// var x8 = new Date();      // A new Date object 
+
+//Literal Method
+// var x1 = {};            // new object
+// var x2 = "";            // new primitive string
+// var x3 = 0;             // new primitive number
+// var x4 = false;         // new primitive boolean
+// var x5 = [];            // new array object
+// var x6 = /()/           // new regexp object
+// var x7 = function(){};  // new function object 
+////////////////////////////////////////////////////////////////
+
+// Javascript Object Prototype
+
+// function Person(first,last,age)
+// {
+//     this.first=first;
+//     this.last=last;
+//     this.age=age;
+// }
+// var person1=new Person("anup","Kumar",25);
+// var person2=new Person("Subham","Kumar",35);
+// //trying to add the new property to existing object Constructor
+// Person.nationality = "English";
+// console.log(person1)
+
+// function Person(first,last,age)
+// {
+//     this.first=first;
+//     this.last=last;
+//     this.nationality="indian";
+// }
+
+// console.log(person1)
+
+//////////////////////////////////////////
+// Adding Properties and Methods to Objects
+
+//JS prototype property allows to add new properties to object constructors
+
+// function Person(first, last, age, eye) {
+//     this.firstName = first;
+//     this.lastName = last;
+//     this.age = age;
+//     this.eyeColor = eye;
+//   }
+  
+//   Person.nationality = "English";
+//   var myFather = new Person("John", "Doe", 50, "blue");
+//   console.log(myFather)
+//   console.log(myFather.nationality)
+//   Person.prototype.nationality = "English";
+//   console.log(myFather)
+//   console.log(myFather.nationality)
+
+//////////////Adding the function 
+// Person.name=function()
+// {
+//     return this.firstName+" "+this.lastName;
+// }
+//   var myFather = new Person("John", "Doe", 50, "blue");
+
+// console.log(myFather.name)
+
+// Person.prototype.name=function()
+// {
+//     return this.firstName+" "+this.lastName;
+// }
+
+// console.log(myFather.name())
+
+// var person = {
+//     firstName: "John",
+//     lastName : "Doe",
+//     language : "EN"
+//   };
+
+// console.log(Object.getOwnPropertyNames(person))
+
+// function DoSomething() {
+    
+// }
+
+// console.log(DoSomething.prototype)
+// function Person(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+  
+//   Person.prototype.sayHello = function() { console.log(`Hello my name is ${this.name}`) }
+  
+//   Person.prototype.sayGoodBye = function() { console.log(`See you again!`) }
+  
+//   Person.prototype.isAdult = function() { return this.age > 18 }
+// var p1=new Person("anup",25)
+// console.log(p1)
+// console.log(p1.sayHello())
+// console.log(p1.sayGoodBye())
+// console.log(p1.isAdult())
+
+// function Professional(name, age, profession) {
+//   Person.call(this, name, age)
+//   this.profession = profession
+// }
+
+/////////////////////////////////////////////////////////////////////
+
+// function outer() {
+//   var a = 100;
+//   var b = 900;
+//   var c = 700;
+//   function inner() {
+//     console.log(a)
+//   }
+//     function inner2() {
+//     console.log(b)
+//   }
+//   return {
+//     inner2,
+//     inner
+//   }
+// }
+
+// const obj = outer()
+// console.log(obj)
+// console.log(obj.inner())
+// console.log(obj.inner2())
+////////////////////////////////////////////////////////////////////////
+
+//Object freeze
+// function Person(name, age) {
+//   return Object.freeze({
+//     sayHello: function() {
+//       console.log(`Hello My name is ${name} and my age is ${age}`)
+//     }
+//   })
+// }
+
+// let p1 = Person('aditya', 28)
+// p1.sayHello()
+
+// p1.name = 'Jhon'
+// p1.sayHello()
+
+
+// function doesSomething() { 
+//   var v1 = 10; 
+//   var v2 = 100;
+
+//   console.log(v1, v2)
+// }
+// doesSomething()
+//////////////////////////////////////////////////////////////////////////////
+function createCounter() {
+  let counter = 0;
+  return Object.freeze({
+    incrementCount: function() {
+      counter += 1;
+    },
+    getCounter: function() {
+      return counter;
+    }
+  })
 }
+console.log(createCounter())
+function counters() {
+  let c1 = createCounter();
+  let c2 = createCounter();
+
+  c1.incrementCount()
+  c1.incrementCount()
+  c1.incrementCount()
+  c1.incrementCount()
+  c1.incrementCount()
+  c1.incrementCount()
+
+  console.log(c1.getCounter())
+
+  c2.incrementCount()
+  c2.incrementCount()
+  c2.incrementCount()
+  c2.incrementCount()
+
+
+  console.log(c2.getCounter())
+}
+counters()
+function dummy() { console.log('Hello') }
+
+counters()
+dummy()

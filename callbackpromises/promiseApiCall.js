4df23a6d99e58cd92e40f1700c87c3f86338095b
himanshuZopsmart <himanshu.agrawal@zopsmart.com>
2021-06-07 10:43:52 +0530

    minor changes

--- callbackpromises/promiseApiCall.js
+++ callbackpromises/promiseApiCall.js
@@ -6,14 +6,16 @@ const fetchData = async()=>{
     let options = {
   
         'method' : 'GET',
-        'url': 'https://api.instantwebtools.net/v1/airlines/8',
+        'url': 'https://api.instantwebtools.net/v1/airlines/9',
          'headers': {}
    };
    try{
 
     let data = await axios(options);
     console.log(data);
-
+    options.url = 'https://api.instantwebtools.net/v1/airlines/10'
+    let data2 = await axios(options);;
+    console.log(data2);
    }
    catch(e)
    {

e1ae5acec741976bb37372d31f68e330e6545de7
anupjha299 <anup.kumar@zopsmart.com>
2021-06-08 14:02:28 +0530

    csvtojson

---
+++ anup_kumar/Day5_node_server_to_app/Csv_to_json/csv_to_json.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+const csvtojson = require('csvtojson');
+csvtojson()
+.fromFile("test.csv")
+.then((jsonData)=>{
+    console.log(jsonData);
+    fs.writeFileSync("output.json",JSON.stringify(jsonData),(err)=>{
+        console.log(err);
+    })
+
+})

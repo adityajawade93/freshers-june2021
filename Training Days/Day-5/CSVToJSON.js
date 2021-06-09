6197e65ed07b58a22cc25a5c7b8d024bd5d02d00
AagamZopSmart <aagam.shah@zopsmart.com>
2021-06-09 16:30:59 +0530

    CSV to JSON, 2 methods added

--- Training Days/Day-5/CSVToJSON.js
+++ Training Days/Day-5/CSVToJSON.js
@@ -1,5 +1,19 @@
 // require csvtojson module
 const CSVToJSON = require('csvtojson');
+const fs = require('fs')
+
+// Method-2
+
+CSVToJSON()
+.fromFile('addresses.csv')
+.then((jsonData) => {
+    console.log(jsonData);
+    const data = JSON.stringify(jsonData);
+    fs.writeFile('addresses.json', data, (err) => {
+        if (err) throw err;
+    });
+});
+
 
 // convert users.csv file to JSON array
 CSVToJSON().fromFile('addresses.csv')
@@ -7,6 +21,7 @@ CSVToJSON().fromFile('addresses.csv')
         // users is a JSON array
         // log the JSON array
         console.log(users);
+
     }).catch(err => {
         // log error if any
         console.log(err);
--- Training Days/Day-5/csv-parser.js
+++ Training Days/Day-5/csv-parser.js
@@ -1,9 +1,13 @@
 const fs = require('fs')
 const csv = require('csv-parser')
 const users = [];
+
 function generateUsername(firstname, lastname) {
     return `${firstname[0]}-${lastname}`.toLowerCase();
 }
+
+// Method-1
+
 fs.createReadStream('addresses.csv')
   .pipe(csv())
   .on('data', function (row) {
@@ -24,26 +28,38 @@ fs.createReadStream('addresses.csv')
   })
   .on('end', function () {
       console.table(users)
-      // TODO: SAVE users data to another file
-      writeToCSVFile(users)
+        // TODO: SAVE users data to another file
+        writeToJSONFile(users)
     })
 
 
-function writeToCSVFile(users) {
-    const filename = 'output.csv';
-    fs.writeFile(filename, extractAsCSV(users), err => {
+function writeToJSONFile(users) {
+    const filename = 'output.json';
+    const data = JSON.stringify(users);
+    fs.writeFile(filename, data, err => {
         if (err) {
-            console.log('Error writing to csv file', err);
+            console.log('Error writing to json file', err);
         } else {
             console.log(`saved as ${filename}`);
         }
     });
 }
+
+// function writeToCSVFile(users) {
+//     const filename = 'output.csv';
+//     fs.writeFile(filename, extractAsCSV(users), err => {
+//         if (err) {
+//             console.log('Error writing to csv file', err);
+//         } else {
+//             console.log(`saved as ${filename}`);
+//         }
+//     });
+// }
     
-function extractAsCSV(users) {
-    const header = ["Username, Password, FirstName, LastName, Address, City"];
-    const rows = users.map(user =>
-        `${user.username}, ${user.password}, ${user.firstname}, ${user.lastname}, ${user.address}, ${user.city}`
-    );
-    return header.concat(rows).join("\n");
-}
+// function extractAsCSV(users) {
+//     const header = ["Username, Password, FirstName, LastName, Address, City"];
+//     const rows = users.map(user =>
+//         `${user.username}, ${user.password}, ${user.firstname}, ${user.lastname}, ${user.address}, ${user.city}`
+//     );
+//     return header.concat(rows).join("\n");
+// }

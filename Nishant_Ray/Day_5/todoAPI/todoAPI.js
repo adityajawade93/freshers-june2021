2a628ed7a56fd63b278162c17f71d3367c46099b
rnishant97 <nishant.ray@zopsmart.com>
2021-06-09 14:04:05 +0530

    todo API

--- Nishant_Ray/Day_5/todoAPI/todoAPI.js
+++ Nishant_Ray/Day_5/todoAPI/todoAPI.js
@@ -27,64 +27,80 @@ http.createServer((req, res) => { // all request will have req, res
     console.log("server started at port", port);
 });
 
+function findTask(id){
+    var i = 0;
+    for (i = 0; i < task_array.length; i++) {
+        if (task_array[i].id == id)
+            break;
+    }
+    if (i == task_array.length) return -1;
+    else return i;
+
+}
+
 let handleRequest = (req, res) => {
     console.log("Got request =>", { method: req.method, path: req.url, contentType: req.headers['content-type'], body: req.body });
-    if(req.method === 'GET' && req.url==='/getdata')
+    if(req.method === 'GET' && req.url==='/todo')
   {
     res.writeHead(200, {'Content-Type': 'application/json'});
     res.end(JSON.stringify(task_array, null, 2));
   }
-   else if (req.method === 'POST' && req.url==='/post') {
+  else if(req.method === 'GET' && req.url.match(/\/todo\/.+/)){
+    var id=req.url.substring(6);
+    let i=findTask(id);
+    if (i === -1) {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.write('Not Found');
+        res.end();
+    }
+    else {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify(task_array[i]));
+    }
+  }
+   else if (req.method === 'POST' && req.url==='/todo') {
     let task = new todo(req.body.title,req.body.status)
     task_array.push(task);
     console.log(task)
     res.writeHead(200,{'content-type':'text/html'})
     res.write('task added')
     res.end()
-    } else if ( req.method === "PUT" && req.url ==='/updatetask' ) {
-        let id=req.body.id;
-        for(let i=0;i<task_array;i++){
-            if(task_array[i].id===id)
-            {
-                if(i===task_array.length)
-                {
-                    res.writeHead(404, { 'Content-Type': 'text/html' });
+    } else if ( req.method === "PUT" && req.url.match(/\/todo\/.+/) ) {
+        var id=req.url.substring(6);
+        let i=findTask(id);
+        if (i === -1) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
             res.write('Not Found');
             res.end();
-                }else{
+        }
+        else {
+            if (req.body.title)
                 task_array[i].title = req.body.title;
+
             task_array[i].status = req.body.status;
+
             res.writeHead(200, { 'Content-Type': 'text/html' });
             res.write('Updated');
             res.end();
-            }
-
-            break;
         }
-        }
-    } else if(req.method === "DELETE" && req.url ==='/deletetask'){
-        let id=req.body.id;
-        for(let i=0;i<task_array;i++){
-            if(task_array[i].id===id)
-            {
-                if(i===task_array.length)
-                {
-                    res.writeHead(404, { 'Content-Type': 'text/html' });
-            res.write('Not Found');
+        
+    } else if(req.method === "DELETE" && req.url.match(/\/todo\/.+/)) {
+        var id=req.url.substring(6);
+        let i=findTask(id);
+        if (i === -1) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.write(' Not Found');
             res.end();
-                }else{
-                    todolist.splice(i,1);
+        }
+        else {
+            task_array.splice(i, 1);
             res.writeHead(200, { 'Content-Type': 'text/html' });
             res.write('deleted');
             res.end();
-            }
-
-            break;
-        }
         }
     }else {
         res.writeHead(404, { 'Content-Type': 'text/html' });
-        res.write('<h2>Not Found2</h2>');
+        res.write('Not Found');
         res.end();
     }
 }
@@ -106,4 +122,3 @@ let attachBodyToRequest = (req, res, callback) => {
         callback(req, res);
     });
 }
-

122a96c179d8f91601e8c888ba84f0cf6a1bdee9
Teekamsk99 <teekam.kalyanwat@zopsmart.com>
2021-06-09 14:36:19 +0530

    adding day5 assignment

--- teekam_singh_kalyanwat/day5/csv_json/csvtojson.js
+++ teekam_singh_kalyanwat/day5/csv_json/csvtojson.js
@@ -1,26 +1,7 @@
 let http = require('http');
 const fs = require('fs');
 const csv = require('csv-parser');
-const students = [];
 
-// Reading csv file
-fs.createReadStream('file.csv')
-    .pipe(csv())
-    .on('data', function (row) {
-
-        const student = {
-            Student_name: row.student_name,
-            Age: row.age,
-            Branch: row.branch,
-        }
-        students.push(student);
-    })
-    .on('end', function () {
-        console.table(students);
-    })
-
-
-// converting and writing csv to json
 
 const csvtojs = require('csvtojson');
 let csv_file = "file.csv";
@@ -29,10 +10,7 @@ csvtojs()
 .fromFile(csv_file)
 .then((jsfile) => {
 
-    console.log(jsfile)
-
-    fs.writeFile('jason_file.js',JSON.stringify(jsfile),'utf-8', (error) =>{
+    fs.writeFile('output_jason_file.json',JSON.stringify(jsfile),'utf-8', (error) =>{
        if(error) console.log(error);
     })
 })
-

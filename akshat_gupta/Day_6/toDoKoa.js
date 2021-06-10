ad4cdbaf92a9ab1f26d913ef2937723facbf2aef
akshatguptazopsmart <akshat.gupta@zopsmart.com>
2021-06-11 03:50:46 +0530

    eslint functionality added

--- akshat_gupta/Day_6/toDoKoa.js
+++ akshat_gupta/Day_6/toDoKoa.js
@@ -1,109 +1,142 @@
-const koa = require('koa')
-const router = require('@koa/router')
+const Koa = require('koa');
+const Router = require('@koa/router');
 const bodyParser = require('koa-bodyparser');
 const uuid = require('uuid');
-let port = 3000;
-var toDoList = {};
-var len = 0;
+const port = 3000;
+const toDoList = {};
+let len = 0;
 
-let toDoApp = new koa();
-let myRouter = new router({
-    prefix: '/todo'
+const toDoApp = new Koa();
+const myRouter = new Router({
+    prefix: '/todo',
 });
 
-const reqSuccess = (ctx) => {
-    ctx.status = 200;
-    ctx.type = 'application/json';
-}
+const response = (ctx, flag, body) => {
+    if (flag) {
+        ctx.status = 200;
+        ctx.type = 'application/json';
+    } else {
+        ctx.status = 404;
+        ctx.type = 'text/plain';
+    }
+    ctx.body = body;
+};
 
 myRouter.get('/', (ctx) => {
-    console.log("Got request =>", { method: ctx.request.method, path: ctx.request.url, body: ctx.request.body });
-    reqSuccess(ctx);
-    if(len === 0)
-        ctx.body = "No entries to display."
-    else
-        ctx.body = JSON.stringify(toDoList);
-})
+    console.log('Got request =>', {
+        method: ctx.request.method,
+        path: ctx.request.url,
+        body: ctx.request.body,
+    });
+    if (len === 0) {
+        response(ctx, true, 'No entries to display.');
+    } else {
+        response(ctx, true, JSON.stringify(toDoList));
+    }
+});
 
 myRouter.get('/:id', (ctx) => {
-    console.log("Got request =>", { method: ctx.request.method, path: ctx.request.url, body: ctx.request.body });
-    let id = ctx.params.id;
+    console.log('Got request =>', {
+        method: ctx.request.method,
+        path: ctx.request.url,
+        body: ctx.request.body,
+    });
+    const id = ctx.params.id;
     // if(id.length > 8) {
     //     ctx.status = 404;
     //     ctx.body = "Id must be less than 8 chracters."
     // }
-    if(toDoList.hasOwnProperty(id)) {
-        reqSuccess(ctx);
-        ctx.body = JSON.stringify(toDoList[id]);
+    if (toDoList.hasOwnProperty(id)) {
+        response(ctx, true, JSON.stringify(toDoList[id]));
+    } else {
+        response(ctx, false,
+                'Entry with Id : ' + id + ' does not exist in database.');
     }
-    else {
-        ctx.status = 404;
-        ctx.body = "Entry with Id : " + id + " does not exist in database."
-    }
-})
+});
 
 myRouter.post('/', (ctx) => {
-    console.log("Got request =>", { method: ctx.request.method, path: ctx.request.url, body: ctx.request.body });
-    let title = ctx.request.body.title;
-    let completed = ctx.request.body.completed;
-    if(title === undefined || completed === undefined) {
-        ctx.status = 404;
-        ctx.body = "Not enough details provided."
+    console.log('Got request =>', {
+        method: ctx.request.method,
+        path: ctx.request.url,
+        body: ctx.request.body,
+    });
+    const title = ctx.request.body.title;
+    const completed = ctx.request.body.completed;
+    if (title === undefined || completed === undefined) {
+        response(ctx, false, 'Not enough details provided.');
+        return;
+    } else if (typeof title !== 'string' || typeof completed !== 'boolean') {
+        response(ctx, false,
+                'Only Strings(title) and Boolean(completed) allowed.');
         return;
     }
-    reqSuccess(ctx);
-    let id = uuid.v4();
-    let date = new Date().toJSON().slice(0,10);
-    let newItem = {
+    const id = uuid.v4();
+    const date = new Date().toJSON().slice(0, 10);
+    const newItem = {
         'id': id,
         'createdDate': date,
         'title': title,
-        'completed': completed
-    }
+        'completed': completed,
+    };
     toDoList[id.toString()] = newItem;
     len++;
-    ctx.body = "Entry added Successfully.";
-})
+    response(ctx, false, 'Entry added Successfully.');
+});
 
 myRouter.put('/:id', (ctx) => {
-    console.log("Got request =>", { method: ctx.request.method, path: ctx.request.url, body: ctx.request.body });
-    let id = ctx.params.id;
-    if(toDoList.hasOwnProperty(id) === false) {
-        ctx.status = 404;
-        ctx.body = "Entry with Id : " + id + " does not exist in database.";
+    console.log('Got request =>', {
+        method: ctx.request.method,
+        path: ctx.request.url,
+        body: ctx.request.body,
+    });
+    const id = ctx.params.id;
+    if (toDoList.hasOwnProperty(id) === false) {
+        response(ctx, true,
+                'Entry with Id : ' + id + ' does not exist in database.');
         return;
     }
-    reqSuccess(ctx);
-    let title = ctx.request.body.title;
-    let completed = ctx.request.body.completed;
-    if(title !== undefined)
+    const title = ctx.request.body.title;
+    const completed = ctx.request.body.completed;
+    if (title === undefined || completed === undefined) {
+        response(ctx, false, 'Not enough details provided.');
+        return;
+    } else if (typeof title !== 'string' || typeof completed !== 'boolean') {
+        response(ctx, false,
+                'Only Strings(title) and Boolean(completed) allowed.');
+        return;
+    }
+    if (title !== undefined) {
         toDoList[id.toString()].title = title;
-    if(completed !== undefined)
+    }
+    if (completed !== undefined) {
         toDoList[id.toString()].completed = completed;
-    ctx.body = "Entry updated Successfully.";
-})
+    }
+    response(ctx, true, 'Entry updated Successfully.');
+});
 
 myRouter.delete('/:id', (ctx) => {
-    console.log("Got request =>", { method: ctx.request.method, path: ctx.request.url, body: ctx.request.body });
-    let id = ctx.params.id;
-    if(toDoList.hasOwnProperty(id) === false) {
-        ctx.status = 404;
-        ctx.body = "Entry with Id : " + id + " does not exist in database.";
+    console.log('Got request =>', {
+        method: ctx.request.method,
+        path: ctx.request.url,
+        body: ctx.request.body,
+    });
+    const id = ctx.params.id;
+    if (toDoList.hasOwnProperty(id) === false) {
+        response(ctx, true,
+                'Entry with Id : ' + id + ' does not exist in database.');
         return;
     }
-    reqSuccess(ctx);
     delete toDoList[id.toString()];
     len--;
-    ctx.body = "Entry deleted Successfully.";
-})
+    response(ctx, true, 'Entry deleted Successfully.');
+});
 
 toDoApp.use(bodyParser());
-toDoApp.use(myRouter.routes())
-toDoApp.use(async ctx => {
-    ctx.status = 404;
-    ctx.body = "Page not found!";
-})
+toDoApp.use(myRouter.routes());
+toDoApp.use(async (ctx) => {
+    response(ctx, false, 'Page not found!');
+});
 
 toDoApp.listen(port, () => {
-    console.log("Server started at",port);
+    console.log('Server started at', port);
 });

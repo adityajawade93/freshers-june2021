dac0072904e7120b23870549a2170698fe3cc7d1
nityamkumarzopsmart <nityam.kumar@zopsmart.com>
2021-06-11 15:06:20 +0530

    added prettier and es-lint

--- nityam__kumar/Day-7/app.js
+++ nityam__kumar/Day-7/app.js
@@ -1,39 +1,46 @@
-const {createTask,getTask,updateTask,deleteTask,getTaskById}=require('./routes_and_controller.js');
-const Koa = require('koa');
-const bodyParser = require('koa-bodyparser');
-const json=require('koa-json');
-const KoaRouter = require('@koa/router');
-const app = new Koa();
-const router = new KoaRouter();
+const Koa = require("koa");
 
+const bodyParser = require("koa-bodyparser");
+
+const KoaRouter = require("@koa/router");
+
+const json = require("koa-json");
+
+const {
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+  getTaskById,
+} = require("./routes_and_controller");
+
+const app = new Koa();
+
+const router = new KoaRouter();
 
 app.use(bodyParser());
 app.use(json());
 app.use(router.routes()).use(router.allowedMethods());
 
-
-
-
-
-router.get('/todo', (ctx, next) => {
-    getTask(ctx);
+router.get("/todo", (ctx, next) => {
+  getTask(ctx);
 });
 
-router.get('/todo/:id', (ctx, next) => {
-    getTaskById(ctx);
+router.get("/todo/:id", (ctx, next) => {
+  getTaskById(ctx);
 });
 
-router.post('/todo', (ctx, next) => {
-    // console.log("fda");
-    createTask(ctx);
+router.post("/todo", (ctx, next) => {
+  // console.log("fda");
+  createTask(ctx);
 });
 
-router.put('/todo/:id', (ctx, next) => {
-    updateTask(ctx);
+router.put("/todo/:id", (ctx, next) => {
+  updateTask(ctx);
 });
 
-router.delete('/todo/:id', (ctx, next) => {
-    deleteTask(ctx);
+router.delete("/todo/:id", (ctx, next) => {
+  deleteTask(ctx);
 });
 
-module.exports=app;
+module.exports = app;
--- nityam__kumar/Day-7/server.js
+++ nityam__kumar/Day-7/server.js
@@ -1,6 +1,7 @@
 const app = require("./app");
+
 const port = 5001;
 
 app.listen(port, () => {
-    console.log('server is active on port', port);
+  console.log("server is active on port", port);
 });

3a653480c2dc87a12e2892cbb8048a3e0ec22a72
anupjha299 <anup.kumar@zopsmart.com>
2021-06-13 16:48:14 +0530

    add get request

--- anup_kumar/Day6 CsvtoJson API/csvToJsonAPI.js
+++ anup_kumar/Day6 CsvtoJson API/csvToJsonAPI.js
@@ -1,24 +1,39 @@
-let http = require('http');
-let url = require('url');
-const CSVToJSON = require('csvtojson');
-const fs = require('fs')
+let fs=require("fs");
+let url=require("url");
+let http=require("http");
+let csvtojson=require("csvtojson");
 
-const port = 3001;
-
-http.createServer((req,res) => {
-    if (req.method === 'GET') {
-        return handleGetReq(req, res)
-    } else if (req.url === '/todo' && req.method === 'POST') {
-        return handlePostReq(req, res)
-    } else if (req.method === 'DELETE') {
-        return handleDeleteReq(req, res)
-    } else if (req.method === 'PUT') {
-        return handlePutReq(req, res)
+const port =3001;
+http.createServer(
+    (req,res)=>
+    {
+        if(req.method=='GET' ||req.method=='POST' ||req.method=='DELETE'||req.method=='PUT')
+        // res.end("correct request");
+        return handlerequest(req,res);
+        else 
+        {
+            res.end("Error occured invalid request");
+        }
     }
-    else{
-        res.end(`{"error occured": "${http.STATUS_CODES[404]}"}`)
-    }
+).listen(port ,()=>
+{
+    console.log("server started at port",port);
 })
-.listen(port, () => {
-    console.log("server started at port", port);
-});
+
+let handlerequest=(req,res)=>
+{
+    if(req.method=="GET")
+    {
+        if(req.url==="/")
+        {
+            res.writeHead(200); // set status code
+            res.end("Welcome to Server");
+        }
+        else if(req.url.match(/\/.+/))
+        {
+            var filename=req.url.substring(1);
+            path='./'+filename+'.csv';
+            console.log(req.url,path)
+        }
+    }
+}

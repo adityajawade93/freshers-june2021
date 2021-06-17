3d9b447cab658cde4ed09605410d4cbd2ca60c64
rnishant97 <nishant.ray@zopsmart.com>
2021-06-17 12:36:24 +0530

    typescript

--- Nishant_Ray/Day_9/passenger_ts/passengers_server.ts
+++ Nishant_Ray/Day_9/passenger_ts/passengers_server.ts
@@ -1,31 +1,43 @@
+import { createContext } from "vm";
+
 const koa=require('koa');
 const koarouter=require("@koa/router");
 const bodyParser = require('koa-bodyparser');
-const passengers=require('./passengers.json');
+const passengers:passenger_data[]=require('./passengers.json');
 
-// var uniq=require('uniqid');
 let app=new koa();
 let router=new koarouter();
-// var task_array=[];
-const port=3001;
+
+const port:number=3001;
+interface passenger_data{
+    _id:string;
+    name:string;
+    trips:number;
+    airline:any[];
+    __v:string;
+}
 
 
-
-router.get('/v1/passengers', (ctx, next) => {
+router.get('/v1/passengers', (ctx:any, next:any) => {
   console.log("Got request =>", {
     method: ctx.request.method,
     path: ctx.request.url,
     body: ctx.request.body,
   });
-      const page = parseInt(ctx.request.query.page);
-  const size = parseInt(ctx.request.query.size);
-  const totalPages=Math.round(passengers.length / size) + 1
+      const page:number = parseInt(ctx.request.query.page);
+  const size :number= parseInt(ctx.request.query.size);
+  const totalPages:number=Math.round(passengers.length / size) + 1
      if(typeof page==="number" && typeof size==="number" && page>=0){
-       const startid=page*size;
-       const endid=Math.min((page+1)*size,passengers.length);
-         const req_data=passengers.slice(startid,endid);
+       const startid:number=page*size;
+       const endid:number=Math.min((page+1)*size,passengers.length);
+         const req_data:passenger_data[]=passengers.slice(startid,endid);
        
-         const data = {
+         const data :{
+             totalPassengers:number;
+             totalPages:number;
+             data:passenger_data[];
+
+         }= {
           totalPassengers: passengers.length,
           totalPages: totalPages,
           data: req_data,

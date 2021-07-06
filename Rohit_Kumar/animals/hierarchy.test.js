21789fc9b0df30ce7dc987ecb7e86326cd5f740a
rohitzop <rohit.kumar@zopsmart.com>
2021-07-06 11:16:32 +0530

    'myschool'

--- Rohit_Kumar/animals/hierarchy.test.js
+++
@@ -1,8 +0,0 @@
-//const { TestWatcher } = require('@jest/core')
-const { animals,Dog,Cat} = require('./Hierarchy')
-
-test('give good result', () =>{
-    var d1 = new Dog('pet','black','Chicken','barks')
-   // const result = d1.barking();
-   expect(d1.barking()).toBe('food is Chicken and do barks');
-});
--- Rohit_Kumar/flatten/flatten.test.js
+++
@@ -1,9 +0,0 @@
-const flatArr = require('./flatten');
-const funFlat = require('./flatten');
-
-
-test('flatten array is found',() =>{
-    let arr = [1,2,[3,4,[5,6],7],8];
-    
-    expect(flatArr(arr)).toEqual([1,2,3,4,5,6,7,8]);
-})

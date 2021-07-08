010f7e707a2e6392c8581e7cd0213c56098cc884
ayushi-2021 <87061222+ayushi-2021@users.noreply.github.com>
2021-07-08 15:54:59 +0530

    Update thirdHighest.test.js

--- ayushi_shrivastava/Day1/thirdHighest.test.js
+++ ayushi_shrivastava/Day1/thirdHighest.test.js
@@ -10,3 +10,10 @@ test('thirdHighest in array [1,2,3,4,5,6,7,8,9,10,11]', ()=>{
 
     expect(num).toBe(9);
 });
+
+test('thirdHighest in array [1,2]', ()=>{
+    const arr = [1,2];
+    const num = thirdHighest(arr);
+
+    expect(num).toBeNull();
+});

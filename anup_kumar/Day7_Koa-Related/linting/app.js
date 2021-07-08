5ea58b363056fcd624c0300f033bce06f6a2090c
anupjha299 <anup.kumar@zopsmart.com>
2021-07-09 00:45:26 +0530

    added school api

--- anup_kumar/Day7_Koa-Related/linting/app.js
+++
@@ -1,9 +0,0 @@
-const name = 'Anup'
-
-const person = {first: name}
-
-console.log(person)
-
-const sayHelloLinting = (fName) => {
-console.log(`Hello linting, ${fName}`);
-};
--- anup_kumar/Day9_typescript_Intro/index.js
+++
@@ -1,4 +0,0 @@
-var operation = function (a, b) {
-    return a * b;
-};
-console.log(operation(5, 6));

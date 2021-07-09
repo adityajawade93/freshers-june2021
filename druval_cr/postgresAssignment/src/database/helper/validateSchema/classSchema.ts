11409c20194e2a8f0dedc85d34ffe465e1e1ac67
unknown <prateek.jaiswal@zopsmart.com>
2021-07-09 14:06:07 +0530

    updated postgres Assignment

--- druval_cr/postgresAssignment/src/database/helper/validateSchema/classSchema.ts
+++
@@ -1,7 +0,0 @@
-import Joi from 'joi';
-
-const classSchema = Joi.object().keys({
-  name: Joi.string().trim().required(),
-});
-
-export default classSchema;
--- druval_cr/postgresAssignment/src/routes/schedule.ts
+++
@@ -1,9 +0,0 @@
-import Router from '@koa/router';
-
-import { addSchedule } from '../controller/schedule';
-
-const router = new Router({ prefix: '/schedule' });
-
-router.post('/', addSchedule);
-
-export default router;

d5d462e223dde84142c4e235899cdb7280a9f3db
tarun Kumar <tarun.kumar@zopsmart.com>
2021-07-23 18:26:18 +0530

    Restructuring directory

--- tarun_kumar_PostgreAssignment/src/validation/mark-validator.ts
+++
@@ -1,9 +0,0 @@
-import Joi from 'joi';
-
-const markSchema = Joi.object().keys({
-    studentid: Joi.string().uuid().required(),
-    subid: Joi.string().uuid().required(),
-    marks: Joi.number().required().positive().min(0).max(100),  //positive integers range 0-100
-});
-
-export default markSchema;
--- tarun_kumar_PostgreAssignment/src/validation/subject-validator.ts
+++
@@ -1,8 +0,0 @@
-import Joi from 'joi';
-
-const subjectSchema = Joi.object().keys({
-
-    name: Joi.string().required().min(3).max(25),
-});
-
-export default subjectSchema;
